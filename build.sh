8046685a634574358d8e195603133dc23810d926
James Browning <jamesb.fe80@gmail.com>
2024-04-15 16:30:21 -0700

    simplify POSIXish build and test procedures

--- build.sh
+++ build.sh
@@ -4,6 +4,5 @@ cargo test
 cbindgen --lang C --output gorse.h
 cp -v target/debug/deps/libgorse.* .
 ./gorse.py -v
-LD_LIBRARY_PATH=. go test
-LD_LIBRARY_PATH=. go build
-LD_LIBRARY_PATH=. ./gorse
+DYLD_LIBRARY_PATH=. LD_LIBRARY_PATH=. go test
+DYLD_LIBRARY_PATH=. LD_LIBRARY_PATH=. go run .
