a0ad20ba648f47bca306b54cfca5b42d6898db8e
James Browning <jamesb.fe80@gmail.com>
2024-04-15 16:29:26 -0700

    go: Fix type in libgorse.go file.

--- libgorse.go
+++ libgorse.go
@@ -14,7 +14,7 @@ import "unsafe"
 func HexToLFP(in_string string) (bool, uint64) {
 	mid_string := C.CString(in_string)
 	defer C.free(unsafe.Pointer(mid_string))
-	mid_fixed_point := C.u64(0)
+	mid_fixed_point := C.uint64_t(0)
 	success := C.chextolfp(mid_string, &mid_fixed_point)
 	return bool(success), uint64(mid_fixed_point)
 }
