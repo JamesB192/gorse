6ea32e23124fb1aa6673c559766dfedb165e0caa
James Browning <JamesB.fe80@gmail.com>
2024-04-17 11:34:06 -0700

    Make build command a little less arcane.

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #! /bin/sh
 
-cargo test
-cbindgen --lang C --output gorse.h
-cp -v target/debug/deps/libgorse.* .
-./gorse.py -v
-DYLD_LIBRARY_PATH=. LD_LIBRARY_PATH=. go test
-DYLD_LIBRARY_PATH=. LD_LIBRARY_PATH=. go run .
+cargo test && \
+[ -n "$(command -v cbindgen)" ] && \
+cbindgen --lang C --output gorse.h && ( \
+cp -v target/debug/deps/libgorse.* . && \
+./gorse.py -v && \
+DYLD_LIBRARY_PATH=. LD_LIBRARY_PATH=. go test && \
+DYLD_LIBRARY_PATH=. LD_LIBRARY_PATH=. go run .) || \
+echo "Failed to run cbindgen"; exit 1
